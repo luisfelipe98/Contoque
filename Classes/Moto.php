26b7aa50a09e94c3e48f322318ba28147520c814
Luis Felipe <luisfelipemm98@gmail.com>
2019-05-22 14:11:00 -0300

    Adicionando as classes

---
+++ Classes/Moto.php
@@ -0,0 +1,18 @@
+<?php
+
+class Moto extends Veiculo {
+
+  private $cilindradas;
+
+  function __construct ($modelo, $ano, $combustivel, $cor, $isNovo, $lugares, $qtdeMin, $qtdeMax, $cilindradas) {
+    parent::__construct ($modelo, $ano, $combustivel, $cor, $isNovo, $lugares, $qtdeMin, $qtdeMax);
+    $this->cilindradas = $cilindradas;
+  }
+
+  public function getCilindradas() {
+    return $this->cilindradas;
+  }
+
+}
+
+?>
