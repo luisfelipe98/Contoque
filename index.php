5e28b857e0e4bdc58a5dadb87f22d2c7572cab9a
DANIEL FELLIPE PALMA DA SILVA <daniel.fpsilva2@senacsp.edu.br>
2019-05-24 21:35:53 -0300

    add view login

--- index.php
+++ index.php
@@ -8,19 +8,23 @@ require 'req.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="vendor/css/site.css">
+  <link rel="stylesheet" href="vendor/css/login.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Signika:400,700&display=swap" rel="stylesheet">
   <title>Contoque</title>
 </head>
 <body>
 
-
-  <form action="" method="post">
-    <h1>Login</h1>
-    <label for="">E-mail</label>
-    <input type="email">
-    <label for="">Senha</label>
-    <input type="pass">
-    <input type="submit" value="Login">
-  </form>
+  <div class="wrapper">
+    <form action="" method="post" autocomplete="off" class="form-login">
+      <h1 class="titulo_login">Login</h1>
+      <label for="email">E-mail</label>
+      <input type="email" name="email" id="email" placeholder="Seu e-mail" autocomplete="off">
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" id="senha" placeholder="Sua senha" autocomplete="off">
+      <input type="submit" value="Login" name="login" class="btn btn_enviar">
+    </form>
+  </div>
 </body>
 </html>
 
