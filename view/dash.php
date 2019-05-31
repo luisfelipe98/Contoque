21cbc035fb8329e8febf8878bb9ea38ecb5a3ae6
Fellipe de Palma <dfellipe7@gmail.com>
2019-05-31 19:59:19 -0300

    Front Dash Inicial

---
+++ view/dash.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#25568d">
+
+    <!-- FONTES / ICONES -->
+    <link href="https://fonts.googleapis.com/css?family=Signika:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../vendor/css/site.css">
+    <link rel="stylesheet" href="../vendor/css/dash.css">
+
+    <title>Contoque | Dashboard</title>
+</head>
+
+<body>
+<div class="wrapper">
+    <aside class="sidebar">
+
+        <header class="logo-menu">
+            <img src="../media/logo-honda-branco.png" alt="Logo SeC SP">
+        </header>
+
+        <div class="usuario-logado">
+        Fellipe
+        </div>
+
+        <nav class="menu-sidebar">
+            <ul class="menu-sidebar__lista">
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Painel</a>
+                </li>
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Veículos</a>
+<!--                    <ul class="submenu__lista">-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="#">Categoria</a>-->
+<!--                    </ul>-->
+                </li>
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Carros</a>
+<!--                    <ul class="submenu__lista">-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="#">Categoria</a>-->
+<!--                    </ul>-->
+                </li>
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Motos</a>
+<!--                    <ul class="submenu__lista">-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="#">Categoria</a>-->
+<!--                    </ul>-->
+                </li>
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Entrada</a>
+                </li>
+                <li class="menu-sidebar__item">
+                    <a class="menu-sidebar__link" href="#">Saída</a>
+<!--                    <ul class="submenu__lista">-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="">Meus Clientes</a>-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="">Cadastro</a></li>-->
+<!--                    </ul>-->
+                </li>
+<!--                <li class="menu-sidebar__item">-->
+<!--                    <a class="menu-sidebar__link" href="#">Sistema</a>-->
+<!--                    <ul class="submenu__lista">-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="#">Alterar Senha</a></li>-->
+<!--                        <li class="submenu__item">-->
+<!--                            <a class="submenu__link" href="#">Sair</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+            </ul>
+        </nav>
+
+    </aside>
+
+    <section class="main-painel">
+
+        <section class="conteudo-painel">
+            <header class="header-painel">
+                <div>Ultimo Acesso:</div>
+                <div class="breadcrumbs">
+                    <!-- <a href="{$PAG_HOME}">Início</a> • {$PAGINA}-->
+                </div>
+                <p>Versão Beta</p>
+            </header>
+        </section>
+    </section>
+</div>
+
+</body>
+</html>
