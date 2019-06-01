ac3d4a00ef8f0e8851009ccc8e77b34d8a0417b3
Murilo_Casanova <mukadcr@gmail.com>
2019-05-31 22:23:46 -0300

    Implementando VerificaLogin.php

--- controler/VerificaLogin.php
+++ controler/VerificaLogin.php
@@ -5,17 +5,34 @@ require '../req.php';
 $login = $_POST['email'];
 $senha = $_POST['senha'];
 
-$query = "SELECT * FROM usuario WHERE email = :email AND senha = :senha";
+try {
 
-$conexao = Conexao::getConnection();
-$stmt = $conexao->prepare($query);
-$stmt->bindValue(":email", $login);
-$stmt->bindValue(":senha", $senha);
-$stmt->execute();
-$lista = $stmt->fetch();
+  $query = "SELECT * FROM usuario WHERE email = :email AND senha = :senha";
 
+  $conexao = Conexao::getConnection();
+  $stmt = $conexao->prepare($query);
+  $stmt->bindValue(":email", $login);
+  $stmt->bindValue(":senha", $senha);
+  $stmt->execute();
+
+  if ($stmt->rowCount() == 1) {
+    $lista = $stmt->fetch();
+    session_start();
+
+  } else {
+    throw new PDOException("Usuário inexistente");
+  }
+
+
+  echo "<pre>";
+  var_dump($lista);
+  echo "</pre>";
+
+} catch (PDOException $e) {
+  echo  $e->getMessage();
+}
 echo "<pre>";
-var_dump($lista);
+var_dump($stmt->rowCount());
 echo "</pre>";
 
 
